a9c0f31276954704e2e96a094af64eb0d0d094f5
Francis Tayag <zyenapz@gmail.com>
2025-02-25 15:48:13 +0800

    Allow updating of game board

--- script.js
+++ script.js
@@ -8,12 +8,21 @@ const Gameboard = (function() {
     const board = Array(ROWS).fill( Array(COLUMNS).fill(AIR) );
 
     const getBoard = () => board;
+    const updateBoard = (marker, row, col) => {
+        if (row >= ROWS || col >= COLUMNS) 
+            return;
+
+        board[row][col] = marker;
+    }
 
     return {
         getBoard,
+        updateBoard,
         MARKER_P1,
         MARKER_P2
     }
 })();
 
+console.log(Gameboard.getBoard());
+console.log(Gameboard.updateBoard(Gameboard.MARKER_P1, 2, 1));
 console.log(Gameboard.getBoard());
