5936e1868392ea542d9a32f1a7a71bad03e859a6
Francis Tayag <zyenapz@gmail.com>
2025-02-25 16:45:01 +0800

    Fix formatting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const Gameboard = (function() {
+const Gameboard = (function () {
     const AIR = 0;
     const MARKER_P1 = 1;
     const MARKER_P2 = 2;
@@ -7,19 +7,19 @@ const Gameboard = (function() {
     const ROWS = SIZE;
 
     const board = [];
-    for(let r=0; r < ROWS; r++) {
+    for (let r = 0; r < ROWS; r++) {
         board[r] = new Array(COLUMNS).fill(0);
     }
 
     const getBoard = () => board;
     const updateBoard = (marker, row, col) => {
-        if (row >= ROWS || col >= COLUMNS) 
+        if (row >= ROWS || col >= COLUMNS)
             return;
 
         board[row][col] = marker;
     }
     const printBoard = () => {
-        for(row of board) {
+        for (row of board) {
             console.log(row);
         }
     }
@@ -36,16 +36,16 @@ const Gameboard = (function() {
     }
 })();
 
-const GameController = (function() {
+const GameController = (function () {
     const MUST_MATCH = 3;
     let currentMarker = Gameboard.MARKER_P1;
     let winner = null;
 
     const checkWinner = (board, currentMarker) => {
         // Vertical check
-        for(let c=0; c < Gameboard.COLUMNS; c++) {
+        for (let c = 0; c < Gameboard.COLUMNS; c++) {
             let matches = 0;
-            for(let r=0; r < Gameboard.ROWS; r++) {
+            for (let r = 0; r < Gameboard.ROWS; r++) {
                 if (board[r][c] === currentMarker) {
                     matches++;
                 }
@@ -60,9 +60,9 @@ const GameController = (function() {
         }
 
         // Horizontal check
-        for(let r=0; r < Gameboard.ROWS; r++) {
+        for (let r = 0; r < Gameboard.ROWS; r++) {
             let matches = 0;
-            for(let c=0; c < Gameboard.COLUMNS; c++) {
+            for (let c = 0; c < Gameboard.COLUMNS; c++) {
                 if (board[r][c] === currentMarker) {
                     matches++;
                 }
@@ -82,7 +82,7 @@ const GameController = (function() {
             () => {
                 let pointer = 0;
                 let matches = 0;
-                while(pointer < Gameboard.SIZE) {
+                while (pointer < Gameboard.SIZE) {
                     if (board[pointer][pointer] === currentMarker)
                         matches++;
                     else
@@ -95,13 +95,13 @@ const GameController = (function() {
                     console.log("Winner at diagonal topleft to bottomright");
             }
         )();
-        
+
         // -- Topright to bottomleft
         (
             () => {
                 let pointer = 0;
                 let matches = 0;
-                while(pointer < Gameboard.SIZE) {
+                while (pointer < Gameboard.SIZE) {
                     const col = (Gameboard.SIZE - 1) - pointer;
 
                     if (board[pointer][col] === currentMarker)
@@ -130,7 +130,7 @@ const GameController = (function() {
         Gameboard.printBoard();
         checkWinner(Gameboard.getBoard(), currentMarker);
     };
-    
+
     return {
         runGame,
     }
