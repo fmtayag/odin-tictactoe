2e9416fa99c773f82a3c7d72c846686c8d4946b5
Francis Tayag <zyenapz@gmail.com>
2025-02-25 16:20:54 +0800

    Implement horizontal and vertical checking of winner

--- script.js
+++ script.js
@@ -28,13 +28,67 @@ const Gameboard = (function() {
         updateBoard,
         printBoard,
         MARKER_P1,
-        MARKER_P2
+        MARKER_P2,
+        COLUMNS,
+        ROWS,
     }
 })();
 
-Gameboard.printBoard();
-Gameboard.updateBoard(Gameboard.MARKER_P1, 0, 1);
-Gameboard.updateBoard(Gameboard.MARKER_P1, 1, 1);
-Gameboard.updateBoard(Gameboard.MARKER_P1, 2, 1);
-console.log("\n");
-Gameboard.printBoard();
+const GameController = (function() {
+    const MUST_MATCH = 3;
+    let currentMarker = Gameboard.MARKER_P1;
+    let winner = null;
+
+    const checkWinner = (board, currentMarker) => {
+        // Vertical check
+        for(let c=0; c < Gameboard.COLUMNS; c++) {
+            let matches = 0;
+            for(let r=0; r < Gameboard.ROWS; r++) {
+                if (board[r][c] === currentMarker) {
+                    matches++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (matches === MUST_MATCH) {
+                console.log(`Found a winner at column ${c + 1}`);
+            }
+        }
+
+        // Horizontal check
+        for(let r=0; r < Gameboard.ROWS; r++) {
+            let matches = 0;
+            for(let c=0; c < Gameboard.COLUMNS; c++) {
+                if (board[r][c] === currentMarker) {
+                    matches++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (matches === MUST_MATCH) {
+                console.log(`Found a winner at row ${r + 1}`);
+            }
+        }
+    }
+    const runGame = () => {
+        Gameboard.printBoard();
+        Gameboard.updateBoard(Gameboard.MARKER_P1, 2, 0);
+        // Gameboard.updateBoard(Gameboard.MARKER_P2, 0, 2);
+        Gameboard.updateBoard(Gameboard.MARKER_P1, 2, 1);
+        // Gameboard.updateBoard(Gameboard.MARKER_P2, 0, 0);
+        Gameboard.updateBoard(Gameboard.MARKER_P1, 2, 2);
+        console.log("\n");
+        Gameboard.printBoard();
+        checkWinner(Gameboard.getBoard(), currentMarker);
+    };
+    
+    return {
+        runGame,
+    }
+})();
+
+GameController.runGame();
