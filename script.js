db151ca01ad1c3284667dc384e7b2b729f733dbd
Francis Tayag <zyenapz@gmail.com>
2025-02-25 15:08:19 +0800

    Create game board

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hello World");
+const Gameboard = (function() {
+    const AIR = 0;
+    const MARKER_P1 = 1;
+    const MARKER_P2 = 2;
+    const COLUMNS = 3;
+    const ROWS = 3;
+
+    const board = Array(ROWS).fill( Array(COLUMNS).fill(AIR) );
+
+    const getBoard = () => board;
+
+    return {
+        getBoard,
+        MARKER_P1,
+        MARKER_P2
+    }
+})();
+
+console.log(Gameboard.getBoard());
